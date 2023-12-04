67162a104e38750f064df83469946180ed4e7fcf
maxime.polese <maxime.polese@le-campus-numerique.fr>
2023-12-04 10:24:08 +0100

    first commit

---
+++ src/main/java/com/example/projetdd_savegame/ProjetDdSaveGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetdd_savegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDdSaveGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDdSaveGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetdd_savegame/ProjetDdSaveGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetdd_savegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDdSaveGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
