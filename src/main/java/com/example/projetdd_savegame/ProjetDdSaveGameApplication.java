bc025ef9a65ffea324f61af5dc6d1675522d1033
maxime.polese <maxime.polese@le-campus-numerique.fr>
2023-12-06 16:17:09 +0100

    sans Eureka

--- src/main/java/com/example/projetdd_savegame/ProjetDdSaveGameApplication.java
+++ src/main/java/com/example/projetdd_savegame/ProjetDdSaveGameApplication.java
@@ -2,10 +2,10 @@ package com.example.projetdd_savegame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class ProjetDdSaveGameApplication {
 
     public static void main(String[] args) {
