ebaaa745e8dc1ac0ec34795136ff115ccc96eb38
maxime.polese <maxime.polese@le-campus-numerique.fr>
2023-12-07 13:59:40 +0100

    DTO

--- src/main/java/com/example/projetdd_savegame/Board.java
+++ src/main/java/com/example/projetdd_savegame/Board.java
@@ -1,6 +1,10 @@
 package com.example.projetdd_savegame;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 public class Board {
@@ -8,13 +12,22 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String board;
+//    @Transient
+//    private List<Case> bd;
 
     public Board() {
     }
 
-    public Board(int id, String board) {
+    public Board(int id, List<Case> bd) throws JsonProcessingException {
         this.id = id;
-        this.board = board;
+        this.board = translate(bd);
+    }
+
+    public String translate(List<Case> bd) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        this.board = objectMapper.writeValueAsString(bd);
+        System.out.println(board);
+        return board;
     }
 
     public int getId() {
---
+++ src/main/java/com/example/projetdd_savegame/BoardDTO.java
@@ -0,0 +1,10 @@
+package com.example.projetdd_savegame;
+
+import java.util.List;
+
+public class BoardDTO {
+
+    private int id;
+    private List<Case> board;
+
+}
